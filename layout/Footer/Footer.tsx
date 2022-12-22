1c177884e6d3b3a0165e48e915683f3e8b86f7e9
Your Name <you@example.com>
2022-12-22 18:13:11 +0500

    CSS Grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,14 @@
+import { Ptag } from '../../components';
 import { FooterProps } from './Footer.props';
-import styles from './Sidebar.module.css';
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props}: FooterProps): JSX.Element => {
     return (
-    <div {...props}>
-        Footer
-    </div>);
+    <footer {...props} className={cn(className, styles.footer)}>
+        <Ptag size='p16'>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</Ptag>
+        <Ptag size='p16'><a href="/">Пользовательское соглашение</a></Ptag>
+        <Ptag size='p16'><a href="/">Политика конфиденциальности</a></Ptag>
+    </footer >);
 }; 
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,20 +3,18 @@ import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
 import { LayoutProps } from './Layout.props';
 
-import styles from './Ptag.module.css';
+import styles from './Layout.module.css';
 import { Sidebar } from './Sidebar/Sidebar';
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
-    return (<>
-        <Header />
-        <div>
-            <Sidebar />
-            <div>
+    return (<div className={styles.wrapper}>
+        <Header className={styles.header} />
+            <Sidebar className={styles.sidebar}/>
+            <div className={styles.body}>
                 {children}
             </div>
-        </div>
-        <Footer />
-    </>);
+        <Footer className={styles.footer}/>
+    </div>);
 };
 
 export const withLayout = <T extends Record<string, unknown>> (Component: FunctionComponent<T>) => {
